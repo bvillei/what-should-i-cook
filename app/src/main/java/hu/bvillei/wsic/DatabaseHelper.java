d79fca9d6bac9c35a402afc56156af38079d250c
Balint <balintvillei@gmail.com>
2020-04-14 23:06:26 +0200

    Create DB and Show all button

---
+++ app/src/main/java/hu/bvillei/wsic/DatabaseHelper.java
@@ -0,0 +1,62 @@
+package hu.bvillei.wsic;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "Food.db";
+    public static final String TABLE_NAME = "food_table";
+    public static final String COL_2 = "NAME";
+    public static final String COL_3 = "VEGETARIAN";
+    public static final String COL_4 = "TYPE";
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, 1);
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE " + TABLE_NAME + " (" +
+                "ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "NAME TEXT NOT NULL, " +
+                "VEGETARIAN INTEGER NOT NULL CHECK (VEGETARIAN IN (0,1)), " +
+                "TYPE TEXT NOT NULL CHECK (TYPE IN ('SOUP', 'MAIN', 'DESSERT')))");
+
+        insertData("Pizza", 1, "MAIN");
+        insertData("Gyros", 0, "MAIN");
+        insertData("Chicken Soup", 0, "SOUP");
+        insertData("Gazpacho", 1, "SOUP");
+        insertData("Pho", 0, "SOUP");
+        insertData("Tiramisu", 1, "DESSERT");
+        insertData("Cake", 1, "DESSERT");
+        insertData("Muffin", 1, "DESSERT");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean insertData(String name, int vegetarian, String type) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2, name);
+        contentValues.put(COL_3, vegetarian);
+        contentValues.put(COL_4, type);
+        return (db.insert(TABLE_NAME, null, contentValues) != -1);
+    }
+
+    public Cursor getAllData() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.rawQuery("select * from " + TABLE_NAME, null);
+    }
+
+}
--- app/src/main/java/hu/bvillei/wsic/MainActivity.java
+++ app/src/main/java/hu/bvillei/wsic/MainActivity.java
@@ -1,7 +1,8 @@
 package hu.bvillei.wsic;
 
-import android.view.View;
+import android.database.Cursor;
 import android.widget.*;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,20 +11,21 @@ import java.util.stream.Collectors;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button rollButton, showButton;
+    DatabaseHelper myDb;
+    Button rollButton, listButton;
     TextView resultsTextView;
     CheckBox vegetarianCheckBox;
     RadioGroup typeRadioGroup;
     List<Food> foodSet;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myDb = new DatabaseHelper(this);
 
         rollButton = findViewById(R.id.rollButton);
-        showButton = findViewById(R.id.showButton);
+        listButton = findViewById(R.id.listButton);
         resultsTextView = findViewById(R.id.resultsTextView);
         vegetarianCheckBox = findViewById(R.id.vegetarianCheckBox);
         typeRadioGroup = findViewById(R.id.typeRadioGroup);
@@ -36,6 +38,35 @@ public class MainActivity extends AppCompatActivity {
         foodSet.add(new Food(5, "Muffin", true, Type.DESSERT));
 
         roll();
+        viewAll();
+    }
+
+    public void viewAll() {
+        listButton.setOnClickListener(
+                v -> {
+                    Cursor res = myDb.getAllData();
+                    if (res.getCount() == 0) {
+                        showMessage("Error", "No data found");
+                        return;
+                    }
+                    StringBuilder buffer = new StringBuilder();
+                    while (res.moveToNext()) {
+                        buffer.append("Id :").append(res.getString(0)).append("\n");
+                        buffer.append("Name :").append(res.getString(1)).append("\n");
+                        buffer.append("Vegetarian :").append(res.getString(2)).append("\n");
+                        buffer.append("Type :").append(res.getString(3)).append("\n");
+                    }
+                    showMessage("Data", buffer.toString());
+                }
+        );
+    }
+
+    public void showMessage(String title, String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
     }
 
     public void roll() {
